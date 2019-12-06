8f5ffae341127569468dd11c0b8fcb137a3aef40
mxrtia <41866138+mxrtia@users.noreply.github.com>
2019-12-06 23:45:38 +0100

    Keys added

--- src/com/company/GamePanel.java
+++ src/com/company/GamePanel.java
@@ -47,6 +47,27 @@ public class GamePanel extends JPanel {
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g.drawImage(Parameters.bgImage, 0, 0, null); //RYSOWANIE TŁA
 
+        //NARYSOWANIE TYMCZASOWEJ KLAWIATURY
+        //g.drawImage(Parameters.klawiatura,194,305,null);
+        g.drawImage(Parameters.keys[0],198,305,null);
+        g.drawImage(Parameters.keys[1], 264, 305, null);
+        g.drawImage(Parameters.keys[2], 287, 304, null);
+        g.drawImage(Parameters.keys[3], 352, 305, null);
+        g.drawImage(Parameters.keys[4], 375, 304, null);
+        g.drawImage(Parameters.keys[5], 464, 305, null);
+        g.drawImage(Parameters.keys[6], 531, 305, null);
+        g.drawImage(Parameters.keys[7], 553, 304, null);
+        g.drawImage(Parameters.keys[8], 620, 305, null);
+        g.drawImage(Parameters.keys[9], 643, 304, null);
+        g.drawImage(Parameters.keys[10], 709, 304, null);
+        g.drawImage(Parameters.keys[11], 731, 304, null);
+        g.drawImage(Parameters.keys[12], 821, 305, null);
+        g.drawImage(Parameters.keys[13], 888, 305, null);
+        g.drawImage(Parameters.keys[14], 910, 304, null);
+        g.drawImage(Parameters.keys[15], 977, 305, null);
+        g.drawImage(Parameters.keys[16], 998, 304, null);
+
+
         //Ustaw kolor dolnego paska Menu i narysuj pasek
         g.setColor(Color.darkGray);
         g.fillRect(0, Height-barHeight, Width, barHeight);
@@ -64,6 +85,7 @@ public class GamePanel extends JPanel {
             g.drawString("Poziom 3", 575, Height-70);
             g.drawString("Poziom 4", 825, Height-70);
             g.drawString("Wyjście", 1075, Height-70);
+
         }
     }
 
--- src/com/company/Parameters.java
+++ src/com/company/Parameters.java
@@ -5,17 +5,46 @@ import javax.swing.ImageIcon;
 
 public class Parameters {
     public static Image bgImage;
-
-    public static void loadInitialImages() {
-        bgImage = loadImage("images/background.png");
-    }
     /**  Zmienna stanu okreĹ›lajÄ…cam czy jest przerwa w grze   */
     public static boolean pause=false;
     /** Zmienna stanu okreĹ›lajÄ…ca czy wybrano menu*/
     public static boolean levelPause=false;
+    /** TABLICA KLAWISZY PIANINA **/
+    public static Image[] keys;
+    /** TYMCZASOWA KLAWIATURA **/
+    public static Image klawiatura;
 
+
+    public static void loadInitialImages() {
+        bgImage = loadImage("images/background.png");
+
+        klawiatura = loadImage("images/piano_keys.png");
+
+
+        keys = new Image[17];
+        keys[0] = loadImage("images/key0.png");
+        keys[1] = loadImage("images/key1.png");
+        keys[2] = loadImage("images/key2.png");
+        keys[3] = loadImage("images/key3.png");
+        keys[4] = loadImage("images/key4.png");
+        keys[5] = loadImage("images/key5.png");
+        keys[6] = loadImage("images/key6.png");
+        keys[7] = loadImage("images/key7.png");
+        keys[8] = loadImage("images/key8.png");
+        keys[9] = loadImage("images/key9.png");
+        keys[10] = loadImage("images/key10.png");
+        keys[11] = loadImage("images/key11.png");
+        keys[12] = loadImage("images/key12.png");
+        keys[13] = loadImage("images/key13.png");
+        keys[14] = loadImage("images/key14.png");
+        keys[15] = loadImage("images/key15.png");
+        keys[16] = loadImage("images/key16.png");
+    }
+
+    //metoda pobierająca obiekt klasy Image na podstawie ścieżki dostępu podanej jako String
     public static Image loadImage(String fileName) {
         return new ImageIcon(fileName).getImage();
+
     }
 
 
